0b67dba7e495155f08c11dbe02babefcdaa16d27
alfiputra <alfiputra.id@gmail.com>
2021-12-24 21:05:44 +0700

    Adding ChatActivity to Database

--- app/src/main/java/id/alfiputra/simplechatapp/UserAdapter.kt
+++ app/src/main/java/id/alfiputra/simplechatapp/UserAdapter.kt
@@ -1,6 +1,7 @@
 package id.alfiputra.simplechatapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +11,6 @@ import androidx.recyclerview.widget.RecyclerView
 class UserAdapter(val context: Context, val userList: ArrayList<User>):
     RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
-    class UserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        val textName = itemView.findViewById<TextView>(R.id.txt_name)
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val view: View = LayoutInflater.from(context).inflate(R.layout.user_layout, parent, false)
         return UserViewHolder(view)
@@ -22,9 +19,23 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>):
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val currentUser = userList[position]
         holder.textName.text = currentUser.name
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ChatActivity::class.java)
+
+            intent.putExtra("name", currentUser.name)
+            intent.putExtra("uid", currentUser.uid)
+
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
         return userList.size
     }
+
+    class UserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val textName = itemView.findViewById<TextView>(R.id.txt_name)
+    }
+
 }
